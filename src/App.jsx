a5b0d69629fb26780b7d1af0893c740960857bfa
beasb <beatriz.sborges4@senacsp.edu.br>
2024-07-12 17:59:15 -0300

    conecte criar front

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './componets/pages/Home'
 import Services from './componets/pages/Services'
 import About from './componets/pages/About'
